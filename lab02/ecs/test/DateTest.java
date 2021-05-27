7c7b8491acf28f016cd98a2f09d5e6b9cf59f34b
jkokkat <jazzimkokkat@gmail.com>
2021-05-27 02:54:00 -0400

    added additional two test cases

--- lab02/ecs/test/DateTest.java
+++ lab02/ecs/test/DateTest.java
@@ -19,67 +19,67 @@ class DateTest {
     Date d = new Date(2005,4,15);
     assertEquals(new Date(2005,4,16), d.nextDate());
   }
-    @Test
+  @Test
   void test_3() {
     Date d = new Date(1901,7,20);
     assertEquals(new Date(1901,7,21), d.nextDate());
   }
-    @Test
+  @Test
   void test_4() {
     Date d = new Date(3456,3,27);
     assertEquals(new Date(3456,3,28), d.nextDate());
   }
-    @Test
+  @Test
   void test_5() {
     Date d = new Date(1500,2,17);
     assertEquals(new Date(1500,2,18), d.nextDate());
   }
-   @Test
+  @Test
   void test_6() {
     Date d = new Date(1700,6,29);
     assertEquals(new Date(1700,6,30), d.nextDate());
   }
-   @Test
+  @Test
   void test_7() {
     Date d = new Date(1800,11,29);
     assertEquals(new Date(1800,11,30), d.nextDate());
   }
-   @Test
+  @Test
   void test_8() {
     Date d = new Date(3453,1,29);
     assertEquals(new Date(3453,1,30), d.nextDate());
   }
-   @Test
+  @Test
   void test_9() {
     Date d = new Date(444,2,29);
     assertEquals(new Date(444,3,1), d.nextDate());
   }
-   @Test
+  @Test
   void test_10() {
     Date d = new Date(2005,4,30);
     assertEquals(new Date(2005,5,1), d.nextDate());
   }
-   @Test
+  @Test
   void test_11() {
     Date d = new Date(3453,1,30);
     assertEquals(new Date(3453,1,31), d.nextDate());
   }
-   @Test
+  @Test
   void test_12() {
     Date d = new Date(3456,3,30);
     assertEquals(new Date(3456,3,31), d.nextDate());
   }
-   @Test
+  @Test
   void test_13() {
     Date d = new Date(1901,7,31);
     assertEquals(new Date(1901,8,1), d.nextDate());
   }
-   @Test
+  @Test
   void test_14() {
     Date d = new Date(3453,1,31);
     assertEquals(new Date(3453,2,1), d.nextDate());
   }
-   @Test
+  @Test
   void test_15() {
     Date d = new Date(3456,12,31);
     assertEquals(new Date(3457,1,1), d.nextDate());
@@ -90,23 +90,34 @@ class DateTest {
         IllegalArgumentException.class,
         () -> new Date(1500,2,31)
       );
-
   }
-  @Test
+ @Test
   void test_17() {
   	assertThrows(
         IllegalArgumentException.class,
         () -> new Date(1500,2,29)
       );
-
   }
-
-  @Test
+ @Test
   void test_18() {
   	assertThrows(
         IllegalArgumentException.class,
         () -> new Date(-1,10,20)
       );
 
+  }
+ @Test
+  void test_19() {
+  	assertThrows(
+        IllegalArgumentException.class,
+        () -> new Date(1458,15,12)
+      );
+  }
+ @Test
+  void test_20() {
+  	assertThrows(
+        IllegalArgumentException.class,
+        () -> new Date(1975,6,-50)
+      );
   }
 }
