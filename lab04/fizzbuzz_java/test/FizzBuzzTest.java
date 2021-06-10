8d5cc229a44c1dd09e69801211e7b14298650898
jkokkat <jazzimkokkat@gmail.com>
2021-06-10 14:27:39 -0400

    Started FizzBuzz.java

---
+++ lab04/fizzbuzz_java/test/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    void firstTest(){
+        assertTrue(true);
+    }
+
+}
