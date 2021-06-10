a0cbffe52912ddb11d80d1cf086c52443903ac2c
jkokkat <jazzimkokkat@gmail.com>
2021-06-10 15:00:47 -0400

    Test 15 Passed with previous tests

--- lab04/fizzbuzz_java/test/FizzBuzzTest.java
+++ lab04/fizzbuzz_java/test/FizzBuzzTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     private String fizzBuzz(int num){
-        if (num % 3 == 0){
+        if (num%3==0 && num%5==0){
+			return "FizzBuzz";
+		}else if (num % 3 == 0){
 			return "Fizz";
 		} else if (num % 5 == 0){
 			return "Buzz";
